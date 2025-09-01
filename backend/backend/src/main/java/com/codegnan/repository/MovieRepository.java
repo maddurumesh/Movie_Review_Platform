cc9f2bd94c40b27c92b361a2bd163e166cf8d210
K.Umesh <your-email@example.com>
2025-09-01 22:55:39 +0530

    Create gh-pages branch for GitHub Pages

--- backend/backend/src/main/java/com/codegnan/repository/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codegnan.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.codegnan.entity.Movie;
-
-public interface MovieRepository extends JpaRepository<Movie ,Long>{
-Movie findByTitle(String title);
-}
